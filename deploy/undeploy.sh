2691e0de5293f9abd9a143916ad8adf83907fc68
SolutionsANZ <solutionsanz2@gmail.com>
2018-11-08 18:44:57 +1100

    Fixing typo when undeploying k8s services

--- deploy/undeploy.sh
+++ deploy/undeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #microservices..
   #apis4atp..
-  kubectl delete -f deploy/kubernetes/apis4harness-ing.yaml >>/tmp/noise.out
-  kubectl delete -f deploy/kubernetes/apis4harness-svc.yaml >>/tmp/noise.out
-  kubectl delete -f deploy/kubernetes/apis4harness-dpl.yaml >>/tmp/noise.out
+  kubectl delete -f kubernetes/apis4harness-ing.yaml >>/tmp/noise.out
+  kubectl delete -f kubernetes/apis4harness-svc.yaml >>/tmp/noise.out
+  kubectl delete -f kubernetes/apis4harness-dpl.yaml >>/tmp/noise.out
   kubectl delete namespace apis4harness >>/tmp/noise.out
